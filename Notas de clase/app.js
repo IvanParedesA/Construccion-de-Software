aa8315d931c534376d1047dea8e32d8bb866f60a
Iván <a01705083@tec.mx>
2023-02-23 12:28:13 -0600

    función para ordenar de mayor a menor

--- Notas de clase/app.js
+++ Notas de clase/app.js
@@ -4,3 +4,12 @@ console.log("hola desde node");
 const filesystem = require('fs');
 
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+//El arreglo se ordena de mayor a menor por la cantidad de tiempo
+for (let item of arreglo){
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
