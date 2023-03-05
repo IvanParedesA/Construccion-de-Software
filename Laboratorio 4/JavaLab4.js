94270ef9ae161ff88a831474671b1552076af0a2
Iván <a01705083@tec.mx>
2023-03-05 17:49:03 -0600

    Segundo Problema

--- Laboratorio 4/JavaLab4.js
+++ Laboratorio 4/JavaLab4.js
@@ -1,9 +1,11 @@
 alert('Bienvenido a mi Laboratorio 4');
 
-//Preguntamos al usuario un número
-const numero = Number(prompt("Ingresa un número"));
-
+//Problema 1
 document.write("Problema 1:")
+
+//Preguntamos al usuario un número
+const numero = Number(prompt("Ingresa un número para generar la tabla"));
+
 //Imprimir base de la tabla
 document.write("<table>");
 
@@ -31,3 +33,33 @@ for(i = 1; i < numero + 1; i++){
 //Cerrar la tabla
 document.write("</table>");
 
+//Problema 2
+document.write("<br> Problema 2: <br>");
+
+//Definimos los números aleatorios
+let random1 = Math.floor(Math.random()*100);
+let random2 = Math.floor(Math.random()*100);
+
+//Definimos la suma
+let suma = random1 + random2;
+
+//Registramos el tiempo en el que empezó a hacer la operación
+let tiempoInicio = new Date();
+
+//Mensaje para que el usuario responda la suma
+let respuesta = Number(prompt("¿Cuánto es " + random1 + " + " + random2 + "?"));
+
+//Registramos el tiempo en el que terminó a hacer la operación
+let tiempoFinal = new Date();
+
+//Diferencia en segundos
+let tiempoDiferencia = tiempoFinal - tiempoInicio;
+let diferenciaSegundos = tiempoDiferencia / 1000;
+
+//Condicionales
+if(respuesta == suma){
+  document.write("¡Felicidades! Lograste hacer la suma en " + diferenciaSegundos + " segundos. <br>");
+}
+else{
+  document.write("No lograste resolver adecuadamente la suma y tardaste " + diferenciaSegundos + " segundos. <br>");
+}
