4d786b8076dff371c97f69d8ff39e0e9b4503b12
Iván <a01705083@tec.mx>
2023-03-11 10:38:46 -0600

    Variables Js

---
+++ Laboratorio 6/lab6script.js
@@ -0,0 +1,5 @@
+var myInput = document.getElementById("psw");
+var minuscula = document.getElementById("minuscula");
+var mayuscula = document.getElementById("mayuscula");
+var numero = document.getElementById("numero");
+var longitud = document.getElementById("longitud")
