142613b02a5d1e7ed120575735d29d7d3c7caaec
Iván <a01705083@tec.mx>
2023-03-12 00:22:28 -0600

    Se calculan los ingredientes

--- Laboratorio 10/appLab10.js
+++ Laboratorio 10/appLab10.js
@@ -9,8 +9,8 @@ const server = http.createServer( (request, response) => {
         response.write("<!DOCTYPE html>");
         response.write("<html>");
         response.write('<head><meta charset="utf-8"></head><body>');
-        response.write("<h1>Agencia Vehícular</h1>");
-        response.write('<a href="/pedir">Pide aquí tu nuevo vehículo</a>')
+        response.write("<h1>Pizzería</h1>");
+        response.write('<a href="/pedir">Pide aquí tu pizza</a>')
         response.write("</body></html>");
         response.end();
 
@@ -20,23 +20,23 @@ const server = http.createServer( (request, response) => {
         response.write("<!DOCTYPE html>");
         response.write("<html>");
         response.write('<head><meta charset="utf-8"></head><body>');
-        response.write("<h1>Agencia Vehícular</h1>");
+        response.write("<h1>Pizzería</h1>");
         response.write('<form action="/pedir" method="POST">');
 
         let form = `
             <fieldset>
-                <legend>Escoge tu nuevo auto:</legend>
+                <legend>Escoge la pizza que desees:</legend>
                 <div>
-                    <input type="checkbox" id="Audi" name="Audi">
-                    <label for="Audi">Audi</label>
+                    <input type="checkbox" id="Peperoni" name="Peperoni">
+                    <label for="Peperoni">Peperoni</label>
                 </div>
                 <div>
-                    <input type="checkbox" id="Bmw" name="Bmw">
-                    <label for="Bmw">Bmw</label>
+                    <input type="checkbox" id="Hawaiana" name="Hawaiana">
+                    <label for="Hawaiana">Hawaiana</label>
                 </div>
                 <div>
-                    <input type="number" id="meses" name="meses" value="0">
-                    <label for="meses">meses de financiamiento</label>
+                    <input type="number" id="pizzas" name="pizzas" value="0" min="0">
+                    <label for="pizzas">Pizzas</label>
                 </div>
             </fieldset>
             <br>
@@ -50,8 +50,55 @@ const server = http.createServer( (request, response) => {
 
     } else if(request.url === "/pedir" && request.method === "POST") {
 
-        response.write("Pedido recibido");
-        response.end();
+        const datos = [];
+
+        request.on('data', (dato) => {
+            console.log(dato);
+            datos.push(dato);
+        });
+
+        return request.on('end', () => {
+            const datos_completos = Buffer.concat(datos).toString();
+            console.log(datos_completos);
+
+            response.setHeader('Content-Type', 'text/html');
+            response.write("<!DOCTYPE html>");
+            response.write("<html>");
+            response.write('<head><meta charset="utf-8"></head><body>');
+            response.write('<h1>Ingredientes</h1>');
+
+            let respuesta = "<ul>";
+
+            const peperoni = datos_completos.split('=')[0];
+            if (peperoni === "peperoni") {
+                respuesta += "<li>peperoni</li>";
+            }
+
+            try {
+                if (datos_completos.split('&')[0].split('=')[0] === "hawaiana" || 
+                    datos_completos-split('&')[1].split('=')[0] === "hawaiana") {
+                        respuesta += "<li>piña</li>";
+                    }
+            } catch (error) {
+
+            }
+
+            const pizzas = datos_completos.split('pizzas=')[1];
+            const numero_pizzas = Number(pizzas);
+
+            if (numero_pizzas > 0) {
+                respuesta += "<li>" + numero_pizzas + " kg. de harina</li>";
+                respuesta += "<li>" + numero_pizzas + " cucharadas de sal fina</li>";
+                respuesta += "<li>" + 30 * numero_pizzas + " gr. de levadura</li>";
+                respuesta += "<li>" + 2 * numero_pizzas + " cucharadas de aceite de oliva</li>";
+            }
+            respuesta += "</ul>";
+
+            response.write(respuesta);
+
+            return response.end();
+        });
+        
 
     } else {
 
@@ -60,7 +107,7 @@ const server = http.createServer( (request, response) => {
         response.write("<!DOCTYPE html>");
         response.write("<html>");
         response.write('<head><meta charset="utf-8"></head><body>');
-        response.write("<h1>Error 404 NOT FOUND. Lo sentimos, el auto que estás buscando no existe</h1>");
+        response.write("<h1>Error 404 NOT FOUND. Lo sentimos, la pizza que estás buscando no existe</h1>");
         response.end();
     }
 
