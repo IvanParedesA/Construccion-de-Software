b4446c19d25bf5d416245e9c99bea1f73757b514
Iván <a01705083@tec.mx>
2023-03-12 20:46:26 -0600

    Se agrega ruta contacto y pregunta

--- Laboratorio 11/app.js
+++ Laboratorio 11/app.js
@@ -16,11 +16,27 @@ app.use((request, response, next) => {
 app.use('/home', (request, response, next) => {
 
     response.send('Bienvenido a casa!');
+    /*Puedes acceder a estas páginas:
+    http://localhost:3000/home
+    http://localhost:3000/saludo
+    http://localhost:3000/pizzas/pedir
+    http://localhost:3000/pregunta/respuesta
+    http://localhost:3000/contacto/biografia
+    */
+});
+
+app.use('/saludo', (request, response, next) => {
+
+    response.send('¡Hola Mundo!');
 });
 
 const pizzasRutas = require('./routes/pizzas.routes');
+const contactoRutas = require('./routes/contacto.routes');
+const preguntaRutas = require('./routes/pregunta.routes');
 
 app.use('/pizzas', pizzasRutas);
+app.use('/contacto',contactoRutas);
+app.use('/pregunta', preguntaRutas);
 
 app.use((request, response, next) => {
     console.log('Otro middleware!');
