774f0c2ee27664c52366b1099f8170893b8655a9
Iván <a01705083@tec.mx>
2023-03-12 18:53:08 -0600

    Crear módulo con rutas de pizzas

--- Laboratorio 11/app.js
+++ Laboratorio 11/app.js
@@ -18,47 +18,9 @@ app.use('/home', (request, response, next) => {
     response.send('Bienvenido a casa!');
 });
 
-app.get('/pedir', (request, response, next) => {
+const pizzasRutas = require('./routes/pizzas.routes');
 
-    let html = `
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Pizzería</h1>
-                <form action="/pedir" method="POST">
-                    <fieldset>
-                        <legend>Escoge tu pizza:</legend>
-                        <div>
-                            <input type="checkbox" id="hawaiana" name="hawaiana">
-                            <label for="hawaiana">Hawaiana</label>
-                        </div>
-                        <div>
-                            <input type"checkbox" id="peperoni" name="peperoni">
-                            <label for="peperoni">Peperoni</label>
-                        </div>
-                        <div>
-                            <input type="number" id="pizzas" name="pizzas" value="0" min="0">
-                            <label for="pizzas"> pizzas</label>
-                        </div>
-                    </fieldset>
-                    <br>
-                    <inputtype="submit" value="pedir">
-                </form>
-            </body>
-        </html>
-    `;
-
-    response.send(html);
-});
-
-app.post('/pedir', (request, response, next) => {
-    console.log(request.body);
-
-    response.send("Pediste " + request.body.hot_cakes + " hot cakes");
-});
+app.use('/pizzas', pizzasRutas);
 
 app.use((request, response, next) => {
     console.log('Otro middleware!');
---
+++ Laboratorio 11/pizzas.routes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/pedir', (request, response, next) => {
+
+    let html = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Pizzería</h1>
+                <form action="/pedir" method="POST">
+                    <fieldset>
+                        <legend>Escoge tu pizza:</legend>
+                        <div>
+                            <input type="checkbox" id="hawaiana" name="hawaiana">
+                            <label for="hawaiana">Hawaiana</label>
+                        </div>
+                        <div>
+                            <input type"checkbox" id="peperoni" name="peperoni">
+                            <label for="peperoni">Peperoni</label>
+                        </div>
+                        <div>
+                            <input type="number" id="pizzas" name="pizzas" value="0" min="0">
+                            <label for="pizzas"> pizzas</label>
+                        </div>
+                    </fieldset>
+                    <br>
+                    <inputtype="submit" value="pedir">
+                </form>
+            </body>
+        </html>
+    `;
+
+    response.send(html);
+});
+
+router.post('/pedir', (request, response, next) => {
+    console.log(request.body);
+
+    response.send("Pediste " + request.body.pizzas + " pizzas");
+});
+
+
+module.exports = router;
