4611ab86e887799fa01d828f34e676fca7f27880
Iván <a01705083@tec.mx>
2023-03-18 17:48:35 -0600

    Preguntas lab3

--- Laboratorio 13/routes/pregunta.routes.js
+++ Laboratorio 13/routes/pregunta.routes.js
@@ -11,11 +11,14 @@ router.get('/respuesta', (request, response, next) => {
             <meta charset="UTF-8">
         </head>
         <body>
-            <h1>Pregunta</h1>
-            <p>Describe el archivo package.json.</p>
-            <p>Es un archivo generado automáticamente cuando se instalan paquetes o dependencias en el proyecto. 
-            Su finalidad es mantener un historial de los paquetes instalados y optimizar la forma en que se generan 
-            las dependencias del proyecto y los contenidos de la carpeta node_modules/ .</p>
+            <h1>Preguntas</h1>
+            <p>¿Qué beneficios encuentras en el estilo MVC?</p>
+            <p>Principalmente que facilita una buena organización de todo el sistema de tu página web y además 
+            porque de esta manera también se puede trabajar en equipo eficazmente dividiendo los roles.</p>
+            <p>¿Encuentras alguna desventaja en el estilo arquitectónico MVC?>
+            <p>Mientras hacía correcciones a mi laboratorio, me percaté de que quizás algunos errores sean 
+            dificiles de encontrar por la cantidad de archivos que debe llevar el sistema, así que diría que 
+            dividir el proyecto de esta manera también le agrega complejidad.
         </body>
     </html>
     `;
