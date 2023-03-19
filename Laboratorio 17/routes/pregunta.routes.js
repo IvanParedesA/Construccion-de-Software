92925aefd0f6a15444f30d5601c50090a7929329
Iván <a01705083@tec.mx>
2023-03-19 00:09:02 -0600

    Preguntas lab 17

--- Laboratorio 17/routes/pregunta.routes.js
+++ Laboratorio 17/routes/pregunta.routes.js
@@ -11,14 +11,16 @@ router.get('/respuesta', (request, response, next) => {
             <meta charset="UTF-8">
         </head>
         <body>
-            <h1>Preguntas Laboratorio 14</h1>
-            <p>¿Qué beneficios encuentras en el estilo MVC?</p>
-            <p>Principalmente que facilita una buena organización de todo el sistema de tu página web y además 
-            porque de esta manera también se puede trabajar en equipo eficazmente dividiendo los roles.</p>
-            <p>¿Encuentras alguna desventaja en el estilo arquitectónico MVC?>
-            <p>Mientras hacía correcciones a mi laboratorio, me percaté de que quizás algunos errores sean 
-            dificiles de encontrar por la cantidad de archivos que debe llevar el sistema, así que diría que 
-            dividir el proyecto de esta manera también le agrega complejidad.
+            <h1>Preguntas Laboratorio 17</h1>
+            <p>¿Qué ventajas tiene escribir el código SQL únicamente en la capa del modelo?</p>
+            <p>Para tener control de los datos que van a entrar y salir de la base de datos y 
+            a la vez no haya ningún problema con otras partes del código. En parte, esto también 
+            representa una medida de seguridad.</p>
+            <p>¿Qué es SQL injection y cómo se puede prevenir?</p>
+            <p>es un ataque de inyección de código utilizado para atacar aplicaciones basadas en datos 
+            en las que se insertan sentencias SQL maliciosas en un campo de entrada para su ejecución.
+            Para prevenirlo, se pueden limitar los privilegios de los usuarios, utilizar procedimientos
+            almacenados, consultas parametrizadas y también implementar seguridad multicapa en el sitio.
         </body>
     </html>
     `;
