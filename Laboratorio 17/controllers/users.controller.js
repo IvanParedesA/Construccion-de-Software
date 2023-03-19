f9caa0f4ba509bc54ce96731b2fa146c058b9453
Iván <a01705083@tec.mx>
2023-03-18 20:39:05 -0600

    Corrección logout

--- Laboratorio 17/controllers/users.controller.js
+++ Laboratorio 17/controllers/users.controller.js
@@ -2,4 +2,4 @@ exports.logout = (request, response, next) => {
     request.session.destroy(() => {
         response.redirect('/pizzas/lista'); //Este código se ejecuta cuando la sesión se elimina
     })
-}
+};
