0dda54725017500b2baccc40494c8188dde192ef
Iván <a01705083@tec.mx>
2023-03-19 20:25:23 -0600

    ruta post signup

--- Laboratorio 18/routes/users.routes.js
+++ Laboratorio 18/routes/users.routes.js
@@ -7,6 +7,8 @@ const usersController = require('../controllers/users.controller');
 
 router.get('/signup', usersController.get_signup);
 
+router.post('/signup', usersController.post_signup);
+
 router.get('/logout', usersController.logout);
 
 module.exports = router;
