ea724fe79331368d51de2802a715f6b026975070
Iván <a01705083@tec.mx>
2023-03-19 20:44:51 -0600

    Cifrado de passwords

--- Laboratorio 18/models/users.model.js
+++ Laboratorio 18/models/users.model.js
@@ -1,4 +1,5 @@
 const db = require('../util/database');
+const bcrypt = require('bcryptjs');
 
 module.exports = class User {
 
@@ -11,9 +12,13 @@ module.exports = class User {
 
     //Método para guardar de manera persistente el nuevo objeto.
     save() {
-        return db.execute(`
-            INSERT INTO usuarios (nombre, username, password)
-            VALUES (?, ?. ?)
-        `, [this.nombre, this.username, this.password]);
+        return bcrypt.hash(this.password,12)
+        .then((password_cifrado) => {
+            return db.execute(`
+                INSERT INTO usuarios (nombre, username, password)
+                VALUES (?, ?. ?)
+            `, [this.nombre, this.username, password_cifrado]);
+        })
+        .catch((error) => {console.log(error) });
     }
 }
