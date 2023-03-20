3c76c0762d744b1850129455e75809427816a1d1
Iván <a01705083@tec.mx>
2023-03-19 21:53:17 -0600

    Preguntas lab 18

--- Laboratorio 18/routes/pregunta.routes.js
+++ Laboratorio 18/routes/pregunta.routes.js
@@ -11,16 +11,14 @@ router.get('/respuesta', (request, response, next) => {
             <meta charset="UTF-8">
         </head>
         <body>
-            <h1>Preguntas Laboratorio 17</h1>
-            <p>¿Qué ventajas tiene escribir el código SQL únicamente en la capa del modelo?</p>
-            <p>Para tener control de los datos que van a entrar y salir de la base de datos y 
-            a la vez no haya ningún problema con otras partes del código. En parte, esto también 
-            representa una medida de seguridad.</p>
-            <p>¿Qué es SQL injection y cómo se puede prevenir?</p>
-            <p>es un ataque de inyección de código utilizado para atacar aplicaciones basadas en datos 
-            en las que se insertan sentencias SQL maliciosas en un campo de entrada para su ejecución.
-            Para prevenirlo, se pueden limitar los privilegios de los usuarios, utilizar procedimientos
-            almacenados, consultas parametrizadas y también implementar seguridad multicapa en el sitio.
+            <h1>Preguntas Laboratorio 18</h1>
+            <p>¿Qué otras formas de autentificación existen?</p>
+            <p>Autenticación QR.</p>
+            <p>Doble Factor de autenticación.</p>
+            <p>OTP SMS.</p>
+            <p>Firma electrónica con OTP SMS.</p>
+            <p>Biometría.</p>
+            <p>Certificado digital.</p>
         </body>
     </html>
     `;
