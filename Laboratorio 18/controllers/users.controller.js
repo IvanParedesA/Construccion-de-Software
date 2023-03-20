5111bb0f99b0734a58f88331c64cded3b4b5f87e
Iván <a01705083@tec.mx>
2023-03-19 20:23:31 -0600

    Registro de usuario en base de datos

--- Laboratorio 18/controllers/users.controller.js
+++ Laboratorio 18/controllers/users.controller.js
@@ -1,5 +1,25 @@
+const User = require('../models/users.model');
+
 exports.get_signup = (request, response, next) => {
     response.render('signup');
+};
+
+exports.post_signup = (request, response, next) => {
+
+    const nuevo_usuario = new User({
+        nombre: request.body.nombre,
+        username: request.body.username,
+        password: request.body.password,
+    });
+
+    nuevo_usuario.save()
+    .then(([rows, fieldData]) => {
+
+        request.session.mensaje = "Usuario registrado.";
+
+        response.redirect('/users/login');
+    })
+    .catch((error) => console.log(error));
 };
 
 exports.logout = (request, response, next) => {
