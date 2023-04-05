22224b7eb524d5855fdf4b20c75c5d9d7121929e
Iván <a01705083@tec.mx>
2023-04-04 23:06:00 -0600

    Modelo busqueda de un valor

--- Laboratorio 24/models/pizzas.model.js
+++ Laboratorio 24/models/pizzas.model.js
@@ -1,74 +1,5 @@
 const db = require('../util/database');
 
-/*
-const pizzas = [
-    {
-        nombre: "hawaiana",
-        imagen: "https://napolicartagena.com/wp-content/uploads/2022/01/La-Pizza-Hawaiana-de-Canada.jpg",
-        descripcion: "Rica pizza de jamón con piña",
-        handle: "@hawaiana",
-        ingredientes: "jamón, piña",
-        precio: "200",
-    },
-    {
-        nombre: "peperoni",
-        imagen: "https://babycocina.com/wp-content/uploads/2021/02/pizza-pepperoni.jpg",
-        descripcion: "Rica pizza de peperoni",
-        handle: "@peperoni",
-        ingredientes: "peperoni",
-        precio: "200",
-    },
-    {
-        nombre: "suprema",
-        imagen: "https://recetinas.com/wp-content/uploads/2022/06/pizza-suprema.jpg",
-        descripcion: "Rica pizza de aceitunas, peperoni, pimiento y carne",
-        handle: "@suprema",
-        ingredientes: "aceitunas, peperoni, pimiento, carne",
-        precio: "200",
-    },
-    {
-        nombre: "mexicana",
-        imagen: "https://recetinas.com/wp-content/uploads/2022/07/pizza-mexicana.jpg",
-        descripcion: "Rica pizza de chorizo, chile y cebolla",
-        handle: "@mexicana",
-        ingredientes: "chorizo, chile, cebolla",
-        precio: "200",
-    },
-    {
-        nombre: "jamón",
-        imagen: "https://imag.bonviveur.com/presentacion-final-de-la-pizza-prosciutto-o-con-jamon-cocido.jpg",
-        descripcion: "Rica pizza de jamón",
-        handle: "@jamon",
-        ingredientes: "jamón",
-        precio: "200",
-    },
-    {
-        nombre: "queso",
-        imagen: "https://live.mrf.io/statics/i/ps/irecetasfaciles.com/wp-content/uploads/2019/08/pizza-de-jamon-queso-y-tocino.jpg?width=1200&enable=upscale",
-        descripcion: "Rica pizza de queso",
-        handle: "@queso",
-        ingredientes: "queso",
-        precio: "200",
-    },
-    {
-        nombre: "champiñones",
-        imagen: "https://cdn7.kiwilimon.com/recetaimagen/38841/50030.jpg",
-        descripcion: "Rica pizza de champiñones",
-        handle: "@champiñones",
-        ingredientes: "champiñones",
-        precio: "200",
-    },
-    {
-        nombre: "salchicha",
-        imagen: "https://comecarne.org/wp-content/uploads/2020/09/pizza_salchicha_xxl.jpg",
-        descripcion: "Rica pizza de salchicha",
-        handle: "@salchicha",
-        ingredientes: "salchicha",
-        precio: "200",
-    },
-];
-*/
-
 module.exports = class Pizza {
     //Constructor de la clase:
     //Se utiliza para crear un nuevo objeto y definir las propiedades del modelo
@@ -98,5 +29,18 @@ module.exports = class Pizza {
             return db.execute(query, [id]);
         }
         return db.execute(query);
+    }
+
+    static find(valor_busqueda) {
+        return db.execute(`
+            SELECT *
+            FROM pizzas
+            WHERE nombre LIKE ? OR descripcion LIKE ? OR handle LIKE ?
+            `,
+            [
+                '%' + valor_busqueda + '%', '%' + valor_busqueda + '%',
+                '%' + valor_busqueda + '%'
+            ]
+        );
     }
 }
